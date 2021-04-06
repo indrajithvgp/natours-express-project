a2a09aa4ba8043a9dd21b741cb2733de69526f68
Indrajith V <indrajithvgp@yahoo.com>
2021-04-06 16:52:57 +0530

    added auth middleware for signup page

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,20 +1,10 @@
 const express = require('express')
 const {getAllUsers, createUser, getUser, updateUser, deleteUser} = require('../controllers/userController')
+const {signUp, login} = require('../controllers/authController')
 const router = express.Router()
 
-/* Route configurations */
-
-{
-    // app.get('/api/v1/tours', getAllTours);
-    
-    // app.get('/api/v1/tours/:id', getTour);
-    
-    // app.post('/api/v1/tours', createTour);
-    
-    // app.patch('/api/v1/tours/:id', updateTour);
-    
-    // app.delete('/api/v1/tours/:id', deleteTour);
-    }
+router.route('/signup').post(signUp)
+router.route('/login').post(login)
 
 router.
     route('/')
