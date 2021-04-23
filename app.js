e31caf2e9712cae6a6079dc7258513018f085188
Indrajith V <indrajithvgp@yahoo.com>
2021-04-23 14:46:56 +0530

    created base pug template for overview page

--- app.js
+++ app.js
@@ -1,17 +1,22 @@
 const express = require('express');
 const { Error } = require('mongoose');
 const rateLimit = require('express-rate-limit')
+const path = require('path')
 const helmet = require('helmet')
 const morgan = require('morgan');
 const mongoSanitize = require('express-mongo-sanitize')
 const xss = require('xss-clean')
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes')
+const viewRouter = require('./routes/viewRoutes')
 const reviewRouter = require('./routes/reviewRoutes')
 const AppError = require('./utils/appError')
 const globalErrorHandler = require('./controllers/errorController')
 
 const app = express();
+
+app.set('view engine', 'pug')
+app.set('views', path.join(__dirname, 'views'))
 
 /* Middlewares */
 app.use(helmet())
@@ -34,13 +39,14 @@ app.use(express.json());
 app.use(mongoSanitize())
 app.use(xss())
 
-app.use(express.static(`${__dirname}/public`))
+app.use(express.static(path.join(__dirname,'public')))
 
 app.use((req, res, next) => {
     req.requestTime = new Date().toISOString()
     next()
 })
 
+app.use('/', viewRouter);
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
 app.use('/api/v1/reviews', reviewRouter)
---
+++ routes/viewRoutes.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const {getOverview, getTour} = require('../controllers/viewController')
+
+const router = express.Router()
+
+// router.get('/', (req, res)=>{
+//     res.status(200).render('base', {
+//         tour:'The Forest Hiker',
+//         user:'Jonas'
+//     })
+// })
+
+router.get('/', getOverview)
+
+router.get('/tour', getTour)
+
+module.exports = router
