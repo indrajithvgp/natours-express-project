b0d017348504dadddd61f8364f408676e6d14a3f
Indrajith V <indrajithvgp@yahoo.com>
2021-04-27 14:53:47 +0530

    building the tour page

--- controllers/viewController.js
+++ controllers/viewController.js
@@ -1,12 +1,23 @@
+const Tour = require('../models/tourModel')
+const catchAsync = require('../utils/catchAsync')
 
-exports.getOverview = (req, res)=>{
+
+exports.getOverview = catchAsync(async(req, res, next)=>{
+    const tours = await Tour.find()
+
     res.status(200).render('overview',{
-        title:'All Tours'
+        title:'All Tours',tours
     })
-}
+})
 
-exports.getTour = (req, res)=>{
-    res.status(200).render('tour',{
-        title:'Forest Hiker'
+exports.getTour = catchAsync(async(req, res, next)=>{
+
+    const tour =  await Tour.findOne({slug:req.params.slug}).populate({
+        path:'reviews',
+        fields:'review rating user'
     })
-}
+    
+    res.status(200).render('tour',{
+        title:'Forest Hiker', tour
+    })
+})
--- routes/viewRoutes.js
+++ routes/viewRoutes.js
@@ -12,6 +12,6 @@ const router = express.Router()
 
 router.get('/', getOverview)
 
-router.get('/tour', getTour)
+router.get('/tour/:slug', getTour)
 
 module.exports = router
