9970b8bf86b7851928f8414b10afeb4cc7eaab6e
Indrajith V <indrajithvgp@yahoo.com>
2021-05-12 14:03:15 +0530

    minor changes in view controller

--- controllers/viewController.js
+++ controllers/viewController.js
@@ -1,5 +1,6 @@
 const Tour = require('../models/tourModel')
 const User = require('../models/userModel')
+const Booking = require('../models/bookingModel')
 const catchAsync = require('../utils/catchAsync')
 const AppError = require('../utils/appError')
 
@@ -65,3 +66,13 @@ exports.updateUserData = catchAsync(async (req, res, next) => {
       user: updatedUser
     });
   });
+
+exports.getMyTours = (req, res, next)=>{
+  const bookings = await Booking.find({user: req.user.id})
+  const tourIds = bookings.map(el => el.tour)
+  const tours = await Tour.find({_id:{$in: tourIds}})
+  res.status(200).render('overview', {
+    title:"My Tours",
+    tours
+  })
+}
--- routes/viewRoutes.js
+++ routes/viewRoutes.js
@@ -1,5 +1,5 @@
 const express = require('express')
-const {getOverview, getTour, getLoginForm, getAccount} = require('../controllers/viewController')
+const {getOverview, getTour, getLoginForm, getAccount, getMyTours} = require('../controllers/viewController')
 const {isLoggedIn, protect} = require('../controllers/authController')
 const {createBookingCheckout} = require('../contollers/bookingController')
 const router = express.Router()
@@ -18,5 +18,7 @@ router.get('/tour/:slug', isLoggedIn, getTour)
 router.get('/login', isLoggedIn, getLoginForm)
 
 router.get('/me', protect, getAccount)
+
+router.get('/my-tours', protect, getMyTours)
 
 module.exports = router
